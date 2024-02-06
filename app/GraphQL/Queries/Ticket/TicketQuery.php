56b99a9ad9c5dfd9a36fc9fa28c0adb4f9a84dfd
Gabriel-SH3 <gabriel.meireles@sh3.com.br>
2024-02-06 09:14:09 -0300

    [Feature] Queries para Tickets.
    Adicionado queries de FindAll e FindById.

---
+++ app/GraphQL/Queries/Ticket/TicketQuery.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries\Ticket;
+
+use App\Models\Ticket;
+use Closure;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\SelectFields;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class TicketQuery extends Query
+{
+    public function authorize($root, array $args, $ctx, ?ResolveInfo $resolveInfo = null, ?Closure $getSelectFields = null): bool
+    {
+        try{
+            $this->auth = JWTAuth::parseToken()->authenticate();
+        } catch(JWTException $e){
+            return false;
+        }
+        return (bool) $this->auth;
+    }
+
+    protected $attributes = [
+        'name' => 'ticket/Tickets',
+        'description' => 'A query'
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Ticket');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
+    {
+        $tickets = Ticket::findOrFail($args['id']);
+
+        return $tickets;
+    }
+}
